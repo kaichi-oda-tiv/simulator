d678c4b3858a2e78f7b3f99ad5e208af36453c13
kaichi.oda <kaichi.oda@tier4.jp>
2019-10-28 17:32:54 +0900

    script name and hierarchy path parse test

---
+++ Assets/BundleScript/Editor/GetAttribute.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+using SimpleJSON;
+
+namespace BundleScript
+{
+    public class GetAttribute
+    {
+        [MenuItem("GameObject/GetAttribute", false, 20)]
+        static void OpenWindow()
+        {
+            var w = new GetAttribute();
+            w.Initialize();
+        }
+
+
+
+        void Initialize()
+        {
+            GetAttributeScripts(Selection.activeGameObject);
+        }
+
+        void GetAttributeScripts(GameObject root)
+        {
+            Dictionary<string, List<string>> attributeDatas = new Dictionary<string, List<string>>();
+            var list = root.GetComponentsInChildren<MonoBehaviour>();
+            foreach (var c in list)
+            {
+                var type = c.GetType();
+                var attr = type.GetCustomAttributes(typeof(AssetBundleScriptAttribute), true);
+                if (attr.Length < 1)
+                {
+                    Debug.Log("Custom Attribute Class Not found...");
+                }
+
+                string tree = "";
+                {
+                    Transform objt = c.transform;
+                    tree = $"{objt.name}";
+                    while (objt.gameObject != root)
+                    {
+                        objt = objt.parent;
+                        tree = $"{objt.name}/{tree}";
+                    }
+                }
+
+                // treeがkeyでattrがvaluesのdic
+                if (attributeDatas.ContainsKey(tree))
+                {
+                    attributeDatas[tree].Add(type.Name);
+                }
+                else
+                {
+                    attributeDatas.Add(tree, new List<string> { type.Name });
+                }
+
+                foreach (var n in attr)
+                {
+                    Debug.Log($"{tree}({c.GetHashCode().ToString("x")}) / {type} has {n.ToString()}");
+                }
+            }
+
+
+            SimpleJSON.JSONNode node = new SimpleJSON.JSONObject();
+            foreach (var kv in attributeDatas)
+            {
+                var arr = new SimpleJSON.JSONArray();
+                kv.Value.Select(x => new JSONString(x)).ToList().ForEach(x => arr.Add(x));
+
+                node.Add(kv.Key, arr);
+            }
+
+            Debug.Log(node.ToString());
+
+            var data = JSON.Parse(node.ToString());
+            foreach (var kv in data.Linq)
+            {
+                string s = $"key: {kv.Key} = {kv.Value.ToString()}";
+                Debug.Log(s);
+            }
+
+        }
+
+    }
+}
---
+++ Assets/BundleScript/Sample/Scripts/OverloadHelloBundleScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverloadHelloBundleScript : HelloBundleScript
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"{this.name}.{this.GetHashCode().ToString("x")}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

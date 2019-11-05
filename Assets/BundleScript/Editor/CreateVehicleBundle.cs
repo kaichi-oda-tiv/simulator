5fc49d2b6eb613c8a445a44dc396119a31908222
kaichi.oda <kaichi.oda@tier4.jp>
2019-11-05 16:54:11 +0900

    [wip] generate dll and remove scripts working

--- Assets/BundleScript/Editor/CreateVehicleBundle.cs
+++ Assets/BundleScript/Editor/CreateVehicleBundle.cs
@@ -21,31 +21,113 @@ namespace BundleScript
                 if (vi != null)
                 {
                     var cvb = new CreateVehicleBundle();
-                    cvb.GenerateBundle(Selection.activeGameObject);
+                    cvb.GenerateBundle2(Selection.activeGameObject);
                 }
             }
+        }
+
+        void GenerateBundle2(GameObject root)
+        {
+            // 元からcopyして
+            // copy側を弄って
+            // 元を退避させて
+            // copyを元のファイル名にして
+            // assetbundle作って
+            // copyを消して
+            // 元を元に戻す
+
+            Debug.Log($"root is {root.GetInstanceID()}");
+
+            var rootPath = AssetDatabase.GetAssetPath(root.GetInstanceID());
+            var copyFile = "copy_" + Path.GetFileName(rootPath);
+            var copyPath = Path.Combine(Path.GetDirectoryName(rootPath), copyFile);
+
+            var tempFile = "temp_" + Path.GetFileName(rootPath);
+            var tempPath = Path.Combine(Path.GetDirectoryName(rootPath), tempFile);
+
+            string log = "";
+
+            log = AssetDatabase.CopyAsset(rootPath, copyPath).ToString();
+            Debug.Log($"CopyAsset({rootPath},{copyPath}) => {log}");
+
+            log = AssetDatabase.RenameAsset(rootPath, Path.GetFileNameWithoutExtension(tempFile));
+            Debug.Log($"RenameAsset({rootPath},{Path.GetFileNameWithoutExtension(tempFile)} => {log}");
+            log = AssetDatabase.RenameAsset(copyPath, Path.GetFileNameWithoutExtension(rootPath));
+            Debug.Log($"RenameAsset({copyPath},{Path.GetFileNameWithoutExtension(rootPath)}) => {log}");
+
+            // main処理
+            var editObj = AssetDatabase.LoadAssetAtPath<GameObject>(rootPath);
+            var ga = new GetAttribute(editObj);
+            var attr = ga.GetAttributeScripts<AssetBundleScriptAttribute>();
+
+            // 2. class名からdllを作る
+            var createdll = new CreateBundleDLL();
+            foreach (var kv in attr)
+            {
+                string s = $"{kv.Key}\n";
+                kv.Value.ForEach(x => { s += $"{x}\n"; });
+                Debug.Log(s);
+                // 3. prefabからclassをremoveする
+                kv.Value.ForEach(x =>
+                {
+                    var t = System.AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).Where(bx => $"{bx.Namespace}.{bx.Name}".EndsWith(x)).FirstOrDefault();
+                    var tname = t == null ? "null!" : t.Name;
+                    Debug.Log($"{x} , {tname}");
+                    var list = editObj.GetComponentsInChildren(t, true);
+                    foreach (var c in list)
+                    {
+                        GameObject.DestroyImmediate(c, true);
+                    }
+                });
+
+                // createdll.CreateDLLSingle(key, "");
+            }
+
+
+            // 後しまつ
+
+            log = AssetDatabase.DeleteAsset(rootPath).ToString();
+            Debug.Log($"DeleteAsset({rootPath}) => {log}");
+
+            log = AssetDatabase.RenameAsset(tempPath, Path.GetFileNameWithoutExtension(rootPath));
+            Debug.Log($"RenameAsset({tempPath},{Path.GetFileNameWithoutExtension(rootPath)}) => {log}");
+
         }
 
 
         void GenerateBundle(GameObject root)
         {
+
             var trueName = root.name;
+            Debug.Log($"truename : {trueName}");
             // -2. rootのcloneを作る
             var rootAssetPath = AssetDatabase.GetAssetPath(root.GetInstanceID());
 
             var cloneFileName = Path.GetFileNameWithoutExtension(rootAssetPath);
             cloneFileName = $"_{cloneFileName}.prefab";
-            var clonePath = Path.Combine($"{Path.GetDirectoryName(rootAssetPath)}", $"{cloneFileName}");
+            var clonePath = Path.Combine(Path.GetDirectoryName(rootAssetPath), cloneFileName);
 
-            Debug.Log($"{root.name} : {rootAssetPath}");
-            Debug.Log($"{clonePath}");
+            Debug.Log($"rootName : {root.name} : {rootAssetPath}");
+            Debug.Log($"clonePath : {clonePath}");
             // -1. rootを退避させる為にrename
             root.name = $"true_{root.name}"; // 今からお前の名前は千だ！
+
+            var trueNameTempPath = Path.Combine(Path.GetDirectoryName(rootAssetPath), root.name);
+
             AssetDatabase.CopyAsset(rootAssetPath, clonePath);
             AssetDatabase.Refresh();
             var editObj = AssetDatabase.LoadAssetAtPath<GameObject>(clonePath);
+            Debug.Log($"editObj = {editObj}");
             // 0. cloneしたprefabをrootの名前に変更
             editObj.name = trueName;
+            var cres = AssetDatabase.RenameAsset(clonePath, Path.GetFileNameWithoutExtension(rootAssetPath));
+            Debug.Log($"cres : {cres}");
+            AssetDatabase.Refresh();
+
+            var tres = AssetDatabase.RenameAsset(rootAssetPath, Path.GetFileNameWithoutExtension(trueNameTempPath));
+            AssetDatabase.Refresh();
+            Debug.Log($"root rename  {root.name} / {tres}");
+
             // 1. 特定のAttributeが付いたclass名とtreeinfoを作成する
             var ga = new GetAttribute(editObj);
             var attr = ga.GetAttributeScripts<AssetBundleScriptAttribute>();
@@ -60,21 +142,24 @@ namespace BundleScript
                 // 3. prefabからclassをremoveする
                 kv.Value.ForEach(x =>
                 {
-                    var t = Type.GetType(x);
+                    var t = System.AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).Where(bx => $"{bx.Namespace}.{bx.Name}".EndsWith(x)).FirstOrDefault();
                     var tname = t == null ? "null!" : t.Name;
                     Debug.Log($"{x} , {tname}");
                     var list = editObj.GetComponentsInChildren(t, true);
                     foreach (var c in list)
                     {
-                        GameObject.Destroy(c);
+                        GameObject.DestroyImmediate(c, true);
                     }
                 });
-                // createdll.CreateDLLSingle(key, "");
 
+                // createdll.CreateDLLSingle(key, "");
             }
 
             //AssetDatabase.DeleteAsset(rootAssetPath);
-            //root.name = trueName;
+            //            AssetDatabase.DeleteAsset(rootAssetPath);
+            //            AssetDatabase.RenameAsset(trueNameTempPath, rootAssetPath);
+
+            AssetDatabase.Refresh();
         }
 
 
--- Assets/BundleScript/Editor/FindComponent.cs
+++ Assets/BundleScript/Editor/FindComponent.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Linq;
 using UnityEngine;
 using UnityEditor;
+using PackageName;
 
 public class FindComponent
 {
@@ -10,19 +12,30 @@ public class FindComponent
     [MenuItem("Assets/FindComponentType", false, 20), MenuItem("GameObject/FindComponentType", false, 20)]
     static void FindComponentType()
     {
+        Debug.Log($"{typeof(CallHello).AssemblyQualifiedName} / {typeof(CallHello).Assembly.FullName}");
         new FindComponent().SearchComponent<CallHello>(Selection.activeGameObject);
-
     }
 
     [MenuItem("Assets/FindComponentString", false, 20), MenuItem("GameObject/FindComponentString", false, 20)]
     static void FindComponentString()
     {
-        new FindComponent().SearchComponentString(Selection.activeGameObject, "CallHello");
+        Debug.Log($"{typeof(CallHello).AssemblyQualifiedName} / {typeof(CallHello).Assembly.FullName}");
+        new FindComponent().SearchComponentString(Selection.activeGameObject, "PackageName.CallHello");
     }
+
+    [MenuItem("Assets/FindComponentStringB", false, 20), MenuItem("GameObject/FindComponentStringB", false, 20)]
+    static void FindComponentStringB()
+    {
+        Debug.Log($"{typeof(CallHello).AssemblyQualifiedName} / {typeof(CallHello).Assembly.FullName}");
+        new FindComponent().SearchComponentStringB(Selection.activeGameObject, "CallHello");
+    }
+
+
 
     [MenuItem("Assets/FindComponentHybrid", false, 20), MenuItem("GameObject/FindComponentHybrid", false, 20)]
     static void FindComponentHybrid()
     {
+        Debug.Log($"{typeof(CallHello).AssemblyQualifiedName} / {typeof(CallHello).Assembly.FullName}");
         var root = Selection.activeGameObject;
 
         var list = root.GetComponentsInChildren(typeof(CallHello), true);
@@ -39,6 +52,17 @@ public class FindComponent
     {
         Debug.Log($"{classname} / " + (System.Type.GetType(classname) == null ? "null!" : "yes"));
         var list = root.GetComponentsInChildren(System.Type.GetType(classname), true);
+        Log(list.Select(x => x.ToString()));
+    }
+
+    void SearchComponentStringB(GameObject root, string classname)
+    {
+        Debug.Log($"{classname} / " + (System.Type.GetType(classname) == null ? "null!" : "yes"));
+
+        var cn = System.AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).Where(x => x.Name.EndsWith(classname)).FirstOrDefault();
+        Debug.Log($"{classname} / " + (cn == null ? "null!" : cn.Name));
+
+        var list = root.GetComponentsInChildren(cn, true);
         Log(list.Select(x => x.ToString()));
     }
 
